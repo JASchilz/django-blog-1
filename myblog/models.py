7567661be79cf38700d972688343354ef8423ae4
Joseph Schilz <joseph@schilz.org>
2018-10-14 16:37:34 -0700

    Use Django's own post_category through table

--- myblog/models.py
+++ myblog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
@@ -12,7 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
+    
 class Category(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
@@ -23,9 +22,4 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
-
-class Categorization(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+       
